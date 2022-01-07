cf1e1aaaf753e6d46a5f8df1f3a2ee822813c068
m2vi <iuvo@gmx.at>
2022-01-07 01:16:18 +0100

    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import detect from '../utils/detect';
 import Silicon from '../public/silicon.svg';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   const [percentage, setPercentage] = useState(0);
@@ -14,6 +15,9 @@ const Home: NextPage = () => {
 
   return (
     <div className='full'>
+      <Head>
+        <title>Apple Silicon Detect</title>
+      </Head>
       {typeof percentage === 'number' && percentage ? (
         <div style={{ position: 'relative' }}>
           <div className='gray' style={{ '--percentage': `${percentage}%` } as any}></div>
